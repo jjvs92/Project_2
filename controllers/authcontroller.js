4b7585112f51238dde412006bfea4c99959d15b5
kstoneham <kevin.g.stoneham@gmail.com>
2018-07-26 18:58:43 -0500

    linted everything

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,28 +1,23 @@
-var exports = module.exports = {}
+var exports = (module.exports = {});
 
+exports.signup = function(req, res) {
+  res.render("signup");
+};
 
-exports.signup = function(req,res){
+exports.signin = function(req, res) {
+  res.render("signin");
+};
 
-	res.render('signup'); 
-
-}
-
-exports.signin = function(req,res){
-
-	res.render('signin'); 
-
-}
-
-exports.dashboard = function(req,res){
-	
-	res.render('dashboard', {userid: req.user.id, userWallet: req.user.wallet, userName: req.user.username}); 
-
-}
-
-exports.logout = function(req,res){
-
-  req.session.destroy(function(err) {
-  res.redirect('/');
+exports.dashboard = function(req, res) {
+  res.render("dashboard", {
+    userid: req.user.id,
+    userWallet: req.user.wallet,
+    userName: req.user.username
   });
+};
 
-}
+exports.logout = function(req, res) {
+  req.session.destroy(function(err) {
+    res.redirect("/");
+  });
+};
