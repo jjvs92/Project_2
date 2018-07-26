ed9a1e22ef93b0802dac88d4bbf0e47e7db83935
danielt812 <danielt812@gmail.com>
2018-07-25 21:22:25 -0500

    flexbox on dashboard/partials

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -14,8 +14,7 @@ exports.signin = function(req,res){
 }
 
 exports.dashboard = function(req,res){
-
-	res.render('dashboard', {userid: req.user.id, userWallet: req.user.wallet}); 
+	res.render('dashboard', {userid: req.user.id, userWallet: req.user.wallet, username: req.user.username}); 
 
 }
 
