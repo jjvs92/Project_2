00a6515621edd0c475871111336236876fa3306e
jjvs92 <julian_jvs92@hotmail.com>
2018-07-28 09:30:05 -0500

    let's fix it please

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@
 var fs        = require("fs");
 var path      = require("path");
 var Sequelize = require("sequelize");
-var env       = process.env.JAWS_DB_URL ? "production" : "development";
+var env       = "production";
 var config    = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db        = {};
