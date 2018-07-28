6b4f72a3aaaeef4dee3479fbcae2d580f41a5b1c
jjvs92 <julian_jvs92@hotmail.com>
2018-07-28 03:30:46 -0500

    going to merge master

--- models/index.js
+++ models/index.js
@@ -3,16 +3,10 @@
 var fs        = require("fs");
 var path      = require("path");
 var Sequelize = require("sequelize");
-var env       = process.env.NODE_ENV || "production";
+var env       = process.env.NODE_ENV || "development";
 var config    = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db        = {};
-
-if(process.env.JAWSDB_URL){
-  sequelize = "production";
-}else{
-  sequelize = process.env.NODE_ENV;
-}
 
 
 fs
